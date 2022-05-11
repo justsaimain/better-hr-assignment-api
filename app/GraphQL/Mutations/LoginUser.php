8406a33d63b30d653697125f4912dbaceaa044b9
Sai Main <saimain.venuslab@gmail.com>
2022-05-11 19:59:35 +0630

    login mutation get token

---
+++ app/GraphQL/Mutations/LoginUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+final class LoginUser
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        $user = User::where('email', $args['email'])->first();
+        if ($user) {
+            if (Hash::check($args['password'], $user->password)) {
+                $token = $user->createToken('User Token')->accessToken;
+                return $token;
+            } else {
+                return "Password mismatch";
+            }
+        } else {
+            return "User does not exist";
+        }
+    }
+}
