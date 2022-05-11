05348b1dbdbdefc4b1641f58e8035235ec757095
Sai Main <saimain.venuslab@gmail.com>
2022-05-11 19:13:05 +0630

    create employee table and factory

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'user_id' => function () {
+                return User::all()->random()->id;
+            }
+        ];
+    }
+}
